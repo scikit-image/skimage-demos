eeb51f4f02828256105ddc11d6f9d07d95094201
Stefan van der Walt <stefanv@berkeley.edu>
2015-11-04 13:41:43 -0800

    Remove lena from fisheye

--- fisheye.py
+++ fisheye.py
@@ -1,9 +1,11 @@
-from skimage import transform, data
+from skimage import transform, data, io
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = data.lena()
+image = io.imread('data/stefan.jpg')
+face = image[:185, 15:]
+
 
 def fisheye(xy):
     center = np.mean(xy, axis=0)
@@ -13,17 +15,17 @@ def fisheye(xy):
     r = np.sqrt(xc**2 + yc**2)
     theta = np.arctan2(yc, xc)
 
-    r = 0.87 * np.exp(r**(1/2.5) / 1.75)
+    r = 0.8 * np.exp(r**(1/2.1) / 1.8)
 
     return np.column_stack((
         r * np.cos(theta), r * np.sin(theta)
         )) + center
 
-out = transform.warp(image, fisheye)
+out = transform.warp(face, fisheye)
 
 f, (ax0, ax1) = plt.subplots(1, 2,
                              subplot_kw=dict(xticks=[], yticks=[]))
-ax0.imshow(image)
+ax0.imshow(face)
 ax1.imshow(out)
 
 plt.show()
